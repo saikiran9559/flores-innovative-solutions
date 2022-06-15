84d6f451e4f745e2d9e86ff543a8503d978dbdb7
saikiran9559 <jettisaikiran9559@gmail.com>
2022-06-15 11:29:25 +0530

    initial

---
+++ script.js
@@ -0,0 +1,33 @@
+
+const myslide = document.querySelectorAll('.myslide');
+let counter = 1;
+function autoSlide() {
+	counter += 1;
+	slidefun(counter);
+}
+function slidefun(n) {
+	let i;
+	for(i = 0;i<myslide.length;i++){
+		myslide[i].style.display = 'none';
+	}
+	if(n > myslide.length){
+	   counter = 1;
+	}
+	if(n < 1){
+	   counter = myslide.length;
+	}
+	myslide[counter-1].style.display = 'block';
+}
+
+slidefun(counter);
+
+let timer = setInterval(autoSlide, 5000);
+
+let menu=false;
+function hidemenu()  {  
+    console.log("hide menu function running..");
+    menu=false;
+    document.getElementById("menu-icon").style.visibility="hidden";  
+}
+
+//slider code
